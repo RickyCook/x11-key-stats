fd437e5c6eff237a5e63bd6fe481b24068df67e7
Ricky Cook <mail@thatpanda.com>
2013-04-05 23:11:54 +1100

    Added "WPM" and max delay to sense AFK

--- stats.pl
+++ stats.pl
@@ -4,7 +4,9 @@ use warnings;
 
 use Time::HiRes qw(time);
 
-use constant AVG_NUM => 100;
+use constant AVG_NUM     => 100;
+use constant TOO_LONG    => 2;
+use constant WORD_LENGTH => 5;
 
 open(KEYS, '-|', './keylogger-X11');
 
@@ -13,13 +15,14 @@ my $avg = 0;
 my $last = undef;
 while (<KEYS>) {
 	my $now = time;
+	my $real_avg_num = keys_to_average();
 	if (defined $last) {
 		my $time_since = $now - $last;
-		my $real_avg_num = keys_to_average();
-		$avg = (($avg * $real_avg_num) + $time_since) / ($real_avg_num + 1);
-		printf "%.3f seconds between keys (average %s)\n", $avg, $real_avg_num;
-	} else {
-		$keys_to_average_count++;
+		if ($time_since < TOO_LONG) {
+			$avg = (($avg * $real_avg_num) + $time_since) / ($real_avg_num + 1);
+			my $wpm = 60 / ($avg * WORD_LENGTH);
+			printf "%.3f seconds between keys = %.1f wpm\n", $avg, $wpm;
+		}
 	}
 	$last = $now;
 }
