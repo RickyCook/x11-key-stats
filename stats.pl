4f3c806c37c43f392e477177946ce15d9fa4cc7e
Ricky Cook <mail@thatpanda.com>
2013-04-07 17:57:15 +1000

    Added ignore and keep alive keys

--- stats.pl
+++ stats.pl
@@ -3,12 +3,32 @@ use strict;
 use warnings;
 
 use Time::HiRes qw(time);
+use List::MoreUtils qw(none any);
 
 use constant AVG_NUM     => 100;
 use constant TOO_LONG    => 2;
 use constant WORD_LENGTH => 5;
 
-my $out_message = defined $ARGV[0] && $ARGV[0] eq '-p' ?
+use constant KEEPALIVE_MATCH => qw(
+	BackSpace
+);
+use constant IGNORED_MATCH => qw(
+	Up
+	Down
+	Left
+	Right
+	Insert
+	Home
+	End
+	Prior
+	Next
+	Pause
+	Scroll_Lock
+	Print
+);
+
+my $parseable = defined $ARGV[0] && $ARGV[0] eq '-p';
+my $out_message = $parseable ?
 	"%.3f %.1f\n" :
 	"%.3f seconds between keys = %.1f wpm\n";
 
@@ -18,19 +38,37 @@ open(KEYS, '-|', './keylogger-X11');
 
 my $keys_to_average_count = 0;
 my $avg = 0;
-my $last = undef;
-while (<KEYS>) {
+my $last_alive;
+my $last_tracked;
+while (my $k  = <KEYS>) {
+	chomp $k;
+
+	if(any { $k =~ /$_/ } IGNORED_MATCH) {
+		if (!$parseable) {
+			print "Ignored matched: $k\n";
+		}
+		next;
+	}
+
 	my $now = time;
 	my $real_avg_num = keys_to_average();
-	if (defined $last) {
-		my $time_since = $now - $last;
-		if ($time_since < TOO_LONG) {
-			$avg = (($avg * $real_avg_num) + $time_since) / ($real_avg_num + 1);
-			my $wpm = 60 / ($avg * WORD_LENGTH);
-			printf $out_message, $avg, $wpm;
+
+	if (none { $k =~ /$_/ } KEEPALIVE_MATCH) {
+		if (defined $last_alive) {
+			my $time_since_alive = $now - $last_alive;
+			my $time_since_tracked = $now - $last_tracked;
+			if ($time_since_alive < TOO_LONG) {
+				$avg = (($avg * $real_avg_num) + $time_since_tracked) / ($real_avg_num + 1);
+				my $wpm = 60 / ($avg * WORD_LENGTH);
+				printf $out_message, $avg, $wpm;
+			}
 		}
+		$last_tracked = $now;
+	} elsif (!$parseable) {
+		print "Keepalive matched: $k\n";
 	}
-	$last = $now;
+
+	$last_alive = $now;
 }
 close KEYS;
 
